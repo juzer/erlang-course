1ef8d73896b20345a600889fb1133bea4b86cdd3
Piotr Nizio <piotr.nizio@poczta.onet.pl>
2017-03-05 14:27:47 +0100

    Function for calculating modes of a list of numbers

--- week2/src/listsexc.erl
+++ week2/src/listsexc.erl
@@ -80,6 +80,53 @@ median_test() ->
   2.5 = median([2, 1, 3, 4]).
 
 
+%% the modes of a list of numbers: this is a list consisting of the numbers
+%% that occur most frequently in the list; if there is is just one,
+%% this will be a list with one element only
+modes([X]) -> [X];
+
+modes(X) ->
+  modes(X, []).
+
+modes([], R) -> max_repetitions(R);
+
+modes([X | Xs], R) ->
+  modes(Xs, calculate_repetitions(X, R)).
+
+calculate_repetitions(X, R) ->
+  case filter(X, R, []) of
+    {R, {}} -> R ++ [{X, 1}]; %% X not found
+    {Rf, {X, N}} -> Rf ++ [{X, N + 1}] %% X with N repetitions found
+  end.
+
+%% If list R contains element X - returns a tuple {R-X, {X, N}}, where N is number of repetitions
+%% If list R does not contain element X - returns a tuple {R, {}}
+filter(_, [], RAcc) ->
+  {RAcc, {}};
+
+filter(X, [R | Rs], RAcc) ->
+  case R of
+    {X, N} -> {RAcc ++ Rs, {X, N}};
+    _ -> filter(X, Rs, RAcc ++ [R])
+  end.
+
+max_repetitions([{X, N} | Rs]) ->
+  max_repetitions(Rs, N, [X]).
+
+max_repetitions([], _, Max) ->
+  Max;
+
+max_repetitions([{X, N} | Rs], NMax, _) when (N > NMax) ->
+  max_repetitions(Rs, N, [X]);
+
+max_repetitions([{X, N} | Rs], NMax, Max) when (N == NMax) ->
+  max_repetitions(Rs, N, Max ++ [X]);
+
+max_repetitions([{_, N} | Rs], NMax, Max) when (N < NMax) ->
+  max_repetitions(Rs, NMax, Max).
 
 
-
+modes_test() ->
+  [1] = modes([1, 1, 2]),
+  [2] = modes([2, 1, 2, 1, 1, 2, 2]),
+  [3, 4] = modes([3, 3, 4, 4, 1]).
